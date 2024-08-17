5db65aedd239b2713d94f59fd8dd4c75045cf711
Connor <crlough@protonmail.com>
2024-08-17 15:26:25 -0700

    export to .csv included

--- core/generate.py
+++ core/generate.py
@@ -1,4 +1,5 @@
 import duckdb
+import os
 from table_defs.application_tables import create_application_tables
 from table_defs.grant_tables import create_grant_tables
 from table_defs.report_tables import create_report_tables
@@ -8,6 +9,12 @@ from data_inserts.report_inserts import generate_report_mock_data
 
 # Connect to a new DuckDB database
 conn = duckdb.connect('grants.duckdb')
+
+# Define the output directory for eventual CSV files
+output_dir = 'output_data'
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
 
 # Open connection and create tables
 tables_list = [
@@ -38,4 +45,12 @@ with duckdb.connect('grants.duckdb') as conn:
     generate_application_mock_data(conn)
     generate_report_mock_data(conn)
 
-    conn.table("reports").show()
+    # conn.table("reports").show()
+
+    # Loop through the tables_list
+    for table_name in tables_list:
+        # Construct the output file path
+        output_file = os.path.join(output_dir, f"{table_name}.csv")
+
+        # Write the table to a CSV file
+        conn.sql(f"SELECT * FROM {table_name}").write_csv(output_file)
